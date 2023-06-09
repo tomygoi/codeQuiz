4ef300e5f332cd448fd83462d9d1392ed1803cb3
Tony Trinh <tonytrinh911@gmail.com>
2023-06-09 15:29:42 -0400

    completed quiz questions array

--- assets/script.js
+++ assets/script.js
@@ -1,109 +1,94 @@
 const questions = [
-    {
-        question: "Which of the following is not a basic javascript data type ?",
-        optionA: "Boolean",
-        optionB: "Integer",
-        optionC: "String",
-        optionD: "Color",
-        correctOption: "optionD"
-    },
+  {
+    question: "Which of the following is not a basic javascript data type ?",
+    optionA: "Boolean",
+    optionB: "Integer",
+    optionC: "String",
+    optionD: "Color",
+    correctOption: "optionD",
+  },
 
+  {
+    question:
+      "What method combines the text of two strings and returns a new string ?",
+    optionA: "add()",
+    optionB: "concat()",
+    optionC: "combine()",
+    optionD: "attach()",
+    correctOption: "optionB",
+  },
 
-    {
-        question: "What method combines the text of two strings and returns a new string ?",
-        optionA: "add()",
-        optionB: "concat()",
-        optionC: "combine()",
-        optionD: "attach()",
-        correctOption: "optionB"
-    },
+  {
+    question: "Arrays in JavaScript can be used to store ________.",
+    optionA: "numbers and strings",
+    optionB: "other arrays",
+    optionC: "booleans",
+    optionD: "all of the above",
+    correctOption: "optionD",
+  },
 
+  {
+    question:
+      "The condition in an if / else statement is enclosed with ______.",
+    optionA: "square brackets",
+    optionB: "curly brackets",
+    optionC: "quotes",
+    optionD: "parenthesis",
+    correctOption: "optionC",
+  },
 
-    {
-        question: "Question ?",
-        optionA: "wrong answer",
-        optionB: "wrong answer",
-        optionC: "wrong answer",
-        optionD: "right answer",
-        correctOption: "optionD"
-    },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is: ",
+    optionA: "terminal/bash",
+    optionB: "for loops",
+    optionC: "JavaScript",
+    optionD: "console.log",
+    correctOption: "optionD",
+  },
 
+  {
+    question: "What keyword is used to define a function in JavaScript?",
+    optionA: "function",
+    optionB: "define",
+    optionC: "func",
+    optionD: "let",
+    correctOption: "optionA",
+  },
 
-    {
-        question: "Question ?",
-        optionA: "wrong answer",
-        optionB: "wrong answer",
-        optionC: "right answer",
-        optionD: "wrong answer",
-        correctOption: "optionC"
-    },
+  {
+    question: "What does the `push()` method do in JavaScript?",
+    optionA: "Adds an element to the beginning of an array",
+    optionB: "Removes an element from the beginning of an array",
+    optionC: "Adds an element to the end of an array",
+    optionD: "Prints a new line",
+    correctOption: "optionC",
+  },
 
+  {
+    question: "What method removes the last element in an array in JavaScript?",
+    optionA: "pop()",
+    optionB: "push()",
+    optionC: "remove()",
+    optionD: "cut()",
+    correctOption: "optionA",
+  },
 
-    {
-        question: "Question?",
-        optionA: "wrong answer",
-        optionB: "wrong answer",
-        optionC: "wrong answer",
-        optionD: "right answer",
-        correctOption: "optionD"
-    },
+  {
+    question: "What is the purpose of the `typeof` operator in JavaScript?",
+    optionA: "it converts the value of a variable",
+    optionB: "it assigns a value of `null`",
+    optionC: "it checks if a function will run",
+    optionD: "it returns the data type of a value",
+    correctOption: "optionD",
+  },
 
-
-    {
-        question: "Question ?",
-        optionA: "right answer",
-        optionB: "wrong answer",
-        optionC: "wrong answer",
-        optionD: "wrong answer",
-        correctOption: "optionA"
-    },
-
-
-    {
-        question: "Question ?",
-        optionA: "wrong answer",
-        optionB: "wrong answer",
-        optionC: "right answer",
-        optionD: "wrong answer",
-        correctOption: "optionC"
-    },
-
-
-    {
-        question: "Question?",
-        optionA: "right answer",
-        optionB: "wrong answer",
-        optionC: "wrong answer",
-        optionD: "wrong answer",
-        correctOption: "optionA"
-    },
-
-
-    {
-        question: "Question ?",
-        optionA: "wrong answer",
-        optionB: "wrong answer",
-        optionC: "wrong answer",
-        optionD: "right answer",
-        correctOption: "optionD"
-    },
-
-
-    {
-        question: "Question ?",
-        optionA: "wrong answer",
-        optionB: "wrong answer",
-        optionC: "wrong answer",
-        optionD: "right answer",
-        correctOption: "optionD"
-    },
-
-
-]
-
-
-
-
-
-
-
+  {
+    question: "What does the modulus oeprator(%) do?",
+    optionA: "performs division",
+    optionB: "raises to an exponent",
+    optionC: "performs multiplication",
+    optionD: "performs division and returns the remainder",
+    correctOption: "optionD",
+  },
+];
